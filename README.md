217595270433d67ad3d9fc6067341982ba333bf3
dubersfeld <dominique42ubersfeld@gmail.com>
2020-06-05 16:42:18 +0200

    Source code added.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # depth-first-search-forest
 Java based demonstration of Depth First Search algorithm using HTML5 Canvas, Javascript and AJAX with animation.
+
+I present here a Java based demonstration of the Depth First Search algorithm.
+
+The algorithm itself is implemented in Java. Javascript is used for initialization and display only.
+
+The browser sends a single Ajax request to the server that sends back a response that contains a collection of all intermediate search results with a new discovery at each step. Then this collection is used for animation.
+
+To launch the demo run the command `mvn spring-boot:run` in project directory. When the application has started open a browser and hit URL `localhost:8080/depth-first-search`
+
+The algorithm starts with a a directed graph and ends with a forest.
+ 
+Here is a screen shot that can be seen during a demo:
+
+![alt text](images/forest.png "TreeForest after search completion")
+
+For a video demo follow this link:
+
+https://youtu.be/42W9TgT1Ei0
+
+For a step-by-step demonstration of the DFS algorithm please visit this repository:
+
+https://github.com/dubersfeld/depth-first-search-step
+
+
+Dominique Ubersfeld, Cachan, France
---
+++ src/main/java/com/dub/spring/controller/DFSController.java
@@ -0,0 +1,118 @@
+package com.dub.spring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dub.spring.depthFirstSearch.DFSGraph;
+import com.dub.spring.depthFirstSearch.DFSResponse;
+import com.dub.spring.depthFirstSearch.DFSVertex;
+import com.dub.spring.depthFirstSearch.GraphInitRequest;
+import com.dub.spring.depthFirstSearch.GraphServices;
+import com.dub.spring.depthFirstSearch.JSONEdge;
+import com.dub.spring.depthFirstSearch.JSONSnapshot;
+import com.dub.spring.depthFirstSearch.JSONVertex;
+import com.dub.spring.depthFirstSearch.SearchRequest;
+
+
+@Controller
+public class DFSController {
+	
+	@Autowired
+	private GraphServices graphServices;
+	
+	/** Initialize graph from JSON object sent by browser
+	 * This method is called only one time 
+	 */
+	@RequestMapping(value="initGraph")
+	@ResponseBody
+	public DFSResponse initGraph(@RequestBody GraphInitRequest message, 
+				HttpServletRequest request) 
+	{	
+		System.out.println("controller: initGraph begin");
+		
+		HttpSession session = request.getSession();
+	
+		if (session.getAttribute("graph") != null) {
+			session.removeAttribute("graph");
+		}
+		
+		List<JSONEdge> jsonEdges = message.getJsonEdges();
+		List<JSONVertex> jsonVertices = message.getJsonVertices();
+	
+		DFSGraph graph = new DFSGraph(jsonVertices.size());
+			
+		for (int i1 = 0; i1 < jsonVertices.size(); i1++) {
+			DFSVertex v = new DFSVertex();
+			v.setName(jsonVertices.get(i1).getName());
+			v.setColor(DFSVertex.Color.BLACK);
+			graph.getVertices()[i1] = v;
+		}
+		
+		for (int i1 = 0; i1 < jsonEdges.size(); i1++) {
+			JSONEdge edge = jsonEdges.get(i1);
+			int from = edge.getFrom();
+			int to = edge.getTo();
+			DFSVertex v1 = (DFSVertex)graph.getVertices()[from];
+			v1.getAdjacency().add(to);
+		}
+		
+		/** Save the new graph to the session context */
+		session.setAttribute("graph", graph);
+			
+		DFSResponse dfsResponse = new DFSResponse();
+		dfsResponse.setStatus(DFSResponse.Status.OK);
+	
+		System.out.println("controller: graph built");
+		
+		// here the graph is ready for the search loop
+		
+		System.out.println("controller: initGraph completed");
+			
+		return dfsResponse;
+	}
+	
+
+	@RequestMapping(value="searchGraph")
+	@ResponseBody
+	public DFSResponse searchGraph(@RequestBody SearchRequest message, 
+											HttpServletRequest request) 
+	{	
+		System.out.println("searchGraph begin");
+		
+		DFSResponse dfsResponse = new DFSResponse();
+		
+		HttpSession session = request.getSession();
+		DFSGraph graph = (DFSGraph)session.getAttribute("graph");
+				
+		System.out.println("searchGraph sator");
+		
+		// snapshots for display
+		List<JSONSnapshot> snapshots = new ArrayList<>();
+				
+		while (!graph.isFinished()) {
+			System.out.println("search while begin");
+			graph.searchStep();
+			JSONSnapshot snapshot = graphServices.graphToJSON(graph);
+			System.out.println("search while ");
+			snapshots.add(snapshot);
+			
+		}// while
+				
+		dfsResponse.setSnapshots(snapshots);
+		dfsResponse.setStatus(DFSResponse.Status.OK);
+		
+		System.out.println("searchGraph return");
+		
+		return dfsResponse;
+	}// searchGraph
+
+}
